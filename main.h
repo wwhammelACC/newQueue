a37963d1f7d165dee20ea53f50b02e3d9e785834
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-05-31 10:41:29 -0500

    added main.h

---
+++ main.h
@@ -0,0 +1,30 @@
+/*
+ * your header here
+ * 
+ */
+
+#ifndef QUEUES_MAIN_H
+#define QUEUES_MAIN_H
+
+// you probably need all these,
+// but if you don't get rid of what you don't need
+#include <stdlib.h>   /* srand, rand */
+#include <time.h>     /* time */
+#include <iostream>   /* cout, endl */
+#include <string>     /* string */
+#include "functions.h"
+
+/*
+ * additional directives here
+ * 
+ */
+// adding queue.h
+#include "queue.h"
+// adding multiplier
+#define MULTIPLIER 1.5 // used to test overflow and underflow
+#define RANDOM_MULTIPLIER 10 // for random testing
+#define CHOICES 6 // used to pick random operations
+
+
+
+#endif /* QUEUES_MAIN_H */
